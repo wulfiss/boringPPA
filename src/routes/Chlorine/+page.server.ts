65a9572b598ffdfe58a77732b593b1d1e437a54f
Julian <96155980+wulfiss@users.noreply.github.com>
2024-06-17 22:28:55 -0300

    refactor: Remove commented out code in Chlorine page server file

---
+++ src/routes/Chlorine/+page.server.ts
@@ -0,0 +1,15 @@
+/* import { supabase } from "$lib/dataBase/supabaseClient";
+import { json, error, type RequestHandler } from "@sveltejs/kit";
+
+export const load = async () => {
+    const { data: measurements, error: dbError } = await supabase
+        .from("freechlorine")
+        .select("*").
+        order("date", { ascending: false });
+
+    if (dbError) {
+        throw new Error(dbError.message);
+    }
+
+    return {measurements};
+} */
